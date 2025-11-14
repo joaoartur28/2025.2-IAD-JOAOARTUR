c5d8a1cd6fe34f8a28f370129e727d44329d2077
joao Artur <joaoartur28@gmail.com>
2025-11-14 13:37:54 -0300

    salvo

---
+++ 1 estágio/atividades2°Cap/larguraxaltura.py
@@ -0,0 +1,18 @@
+largura = 17
+altura = 12.0
+
+a = largura//2
+print(a)
+print(type(a))
+
+b = largura/2.0
+print(b)
+print(type(b))
+
+c = altura/3
+print(c)
+print(type(c))
+
+d = 1 + 2 * 5
+print(d)
+print(type(d))
---
+++ 1 estágio/atividades2°Cap/temperatura.py
@@ -0,0 +1,5 @@
+celsius = float(input("digite a temperatura em celsius: "))
+
+fahrenheit = (celsius * 9/5) + 32
+
+print(f"{celsius}°C equivalem a {fahrenheit:f}°F")
