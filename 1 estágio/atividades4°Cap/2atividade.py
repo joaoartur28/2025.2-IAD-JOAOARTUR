1aec505b11b2de808d474f1ef6b3263ee563a46e
joao Artur <joaoartur28@gmail.com>
2025-11-25 14:30:37 -0300

    tarefas

---
+++ 1 estágio/atividades4°Cap/2atividade.py
@@ -0,0 +1,9 @@
+def print_LetraDeMusica():
+    print("Eu sou um lenhador, e eu estou bem.")
+    print("Eu durmo a noite toda e trabalho o dia todo.")
+
+def repetir_LetraDeMusica():
+    print_LetraDeMusica()
+    print_LetraDeMusica()
+
+repetir_LetraDeMusica()
