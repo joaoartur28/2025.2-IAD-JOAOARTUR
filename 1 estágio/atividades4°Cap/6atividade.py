61834701677fc26a5ef9b2ff73ce520dd9e83499
joao Artur <joaoartur28@gmail.com>
2025-11-25 15:22:58 -0300

    fim

--- 1 estágio/atividades4°Cap/6atividade.py
+++ 1 estágio/atividades4°Cap/6atividade.py
@@ -1,3 +1,7 @@
+# def serve para definir de maneira geral alguns nomes antes, sem precisar sair definindo nome por nome.
+# Além disso, quando um termo é definido com def no inicio, as palavras utilizadas no inicio dentro do parênteses são automaticamente substituidas
+# no final quando o mesmo termo utilizado é citado com outras palavras dentro do parênteses.
+
 def calculoPagamento(horas, taxaHora):
     if horas > 40:
         valor_taxa = horas - 40
