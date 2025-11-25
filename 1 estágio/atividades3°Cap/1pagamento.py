be0e12abefcfa68d63dce188cad4c7613d1eb24c
joao Artur <joaoartur28@gmail.com>
2025-11-25 11:26:01 -0300

    mua

--- 1 estágio/atividades3°Cap/1pagamento.py
+++ 1 estágio/atividades3°Cap/1pagamento.py
@@ -1,4 +1,15 @@
+funcionário = input("você é funcionário? ")
 hora = float(input("digite as horas: "))
 taxa = float(input("digite a taxa: "))
-valor = hora * taxa
-print(valor)
+if funcionário == "não":
+    valor1 = hora * taxa
+    print(valor1)
+elif funcionário == "sim":
+    if hora >= 40:
+        valor_taxa = ((hora - 40) * taxa * 1.5)
+        valor_integral = 40 * taxa
+        valor_total = valor_integral + valor_taxa
+        print(valor_total)
+    elif hora < 40:
+        valor1 = hora * taxa
+        print(valor1)
