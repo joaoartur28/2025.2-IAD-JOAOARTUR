2dfa95a22b57b44a1d0971bcae3ad791fc7edf3a
joao Artur <joaoartur28@gmail.com>
2025-11-25 14:38:11 -0300

    respostas

--- 1 estágio/atividades4°Cap/2atividade.py
+++ 1 estágio/atividades4°Cap/2atividade.py
@@ -1,3 +1,5 @@
+repetir_LetraDeMusica()
+
 def print_LetraDeMusica():
     print("Eu sou um lenhador, e eu estou bem.")
     print("Eu durmo a noite toda e trabalho o dia todo.")
@@ -6,4 +8,5 @@ def repetir_LetraDeMusica():
     print_LetraDeMusica()
     print_LetraDeMusica()
 
-repetir_LetraDeMusica()
+# A mensagem de erro que aparece é: Name "repetir_LetraDeMusica" 
+# is not defined
--- 1 estágio/atividades4°Cap/base2e3.py
+++ 1 estágio/atividades4°Cap/base2e3.py
@@ -1,5 +1,3 @@
-repetir_LetraDeMusica()
-
 def print_LetraDeMusica():
     print("Eu sou um lenhador, e eu estou bem.")
     print("Eu durmo a noite toda e trabalho o dia todo.")
@@ -8,5 +6,4 @@ def repetir_LetraDeMusica():
     print_LetraDeMusica()
     print_LetraDeMusica()
 
-# A mensagem de erro que aparece é: Name "repetir_LetraDeMusica" 
-# is not defined
+repetir_LetraDeMusica()
