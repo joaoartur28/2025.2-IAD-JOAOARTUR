9a44e1157434bd3d5b4023a10b0e96c92d3f3b8f
joao Artur <joaoartur28@gmail.com>
2025-11-28 14:37:19 -0300

    feito

--- Testes/testes.py
+++ Testes/testes.py
@@ -1,9 +1,18 @@
+soma = 0
+quantidade = 0
+
 while True:
-    valor = input("digite um valor:")
+    valor = input("digite um valor: ")
+
     if valor == "pronto":
         break
 
     try:
-        float(valor)
+        numero = float(valor)
+        soma = soma + numero
+        quantidade = quantidade + 1
     except:
         print("Entrada inválida, digite um valor numérico")
+        continue
+
+
