752bde7018b3174dd75cf3956a3b2c3ede694611
joao Artur <joaoartur28@gmail.com>
2025-12-14 00:47:44 -0300

    feito

---
+++ 1 estágio/atividades7°Cap/como_chamar_um_arquivo.py
@@ -0,0 +1,7 @@
+arquivo = open('D:/Documents/GITHUB/2025.2-IAD-JOAOARTUR-1/1 estágio/atividades7°Cap/mbox.txt')
+print(arquivo)
+
+# 1 - Primeiramente se acha o local onde o texto está armazenado, para isso vá no texto, aperte 
+# o botão direito e clique em "copy path"
+
+# 2 - Depois tire todas as barras '\' que separam os termos e substitua por '/'
